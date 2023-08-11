da5ace8c76c71716046390e671a1a963114042c6
Chutacos <114375726+Chutacos@users.noreply.github.com>
2023-08-11 01:39:39 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,37 +1,54 @@
-package SimpleMethod;
+package com.company;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Main {
+
     private static HttpURLConnection connection;
 
-    private static void main(String[] args) {
+    public static void main(String[] args) {
+        //Method 1: Java.net.httpURLconnection
 
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/albums")).build();
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenApply(Main::parse)
-                .join();
-    }
-
-    public static String parse(String responseBody) {
-        JSONArray albums = new JSONArray(responseBody);
-        for(int i = 0; i < albums.length(); i++) {
-            JSONObject album = albums.getJSONObject(i);
-            int id = album.getInt("id");
-            int userID = album.getInt("userID");
-            String title = album.getString("title");
-            System.out.println(id + " " + title + " " + userID);
+         BufferedReader reader;
+        String line;
+        StringBuffer responseContent = new StringBuffer();
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/albums");
+            connection = (HttpURLConnection) url.openConnection();
+            // Request setup
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            int status = connection.getResponseCode();
+            // System.out.println(status);
+            if(status > 299) {
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+                while((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            } else {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            System.out.println(responseContent.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return null;
+        finally {
+            connection.disconnect();
+        }
     }
 }
